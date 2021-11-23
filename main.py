cb3ffaa4fb89f4afd5441f9f63f325cfca9bcbe5
Justin D-Z <justindz@gmail.com>
2021-11-22 22:26:39 -0500

    Added /rent command to check when rent is due for an account.

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ client = Client(intents=Intents.all())
 slash = SlashCommand(client, sync_commands=True)
 servers = secrets.servers
 
-
 # Register Slash Commands
 quest_choices = []
 
@@ -85,9 +84,54 @@ async def timer(ctx: SlashContext, account: str, quest: str):
                                     quest_timer = datetime.fromtimestamp(result_timer['last_Time_Completed'])
                                     is_timer_up = quest_timer.today() - quest_timer > timedelta(quests[quest][1])
                                     # TODO are we at the 27 day maximum of 4 for Stipends?
-                                    await ctx.author.send(f'- {character_name} {quest_timer} {":green_circle:" if is_timer_up else ":red_square:"}')
+                                    await ctx.author.send(
+                                        f'- {character_name} {quest_timer} {":green_circle:" if is_timer_up else ":red_square:"}')
                                 else:
                                     await ctx.author.send(f'- {character_name} N/A')
+            else:
+                await ctx.author.send(f'Account {account} not found on this ACE server.')
+
+
+@slash.slash(name="rent",
+             description="Check rent for the housing on the specified account.",
+             options=[
+                 create_option(
+                     name='account',
+                     description='The username for your ACE account.',
+                     option_type=3,
+                     required=True
+                 )
+             ],
+             guild_ids=servers)
+async def rent(ctx: SlashContext, account: str):
+    with accounts_db:
+        with accounts_db.cursor() as cursor:
+            sql = 'SELECT `accountId` FROM `account` WHERE `accountName`=%s'
+            cursor.execute(sql, (account.lower().strip(),))
+            result_account_id = cursor.fetchone()
+
+            if result_account_id is not None:
+                account_id = result_account_id['accountId']
+
+                with characters_db:
+                    with characters_db.cursor() as cursor2:
+                        sql = f'SELECT `id` FROM `character` WHERE `account_Id`={account_id}'
+                        cursor2.execute(sql)
+                        result_character = cursor2.fetchone()
+
+                        if result_character is None:
+                            await ctx.author.send(f'Account {account} has no characters.')
+                        else:
+                            character_id = result_character['id']
+                            sql = f'SELECT `value` FROM `biota_properties_int` WHERE `object_Id`={character_id} AND `type`=9011'
+                            cursor2.execute(sql)
+                            result_timer = cursor2.fetchone()
+
+                            if result_timer is not None:
+                                rent_timer = datetime.fromtimestamp(result_timer['value']).strftime('%B %d %Y %H:%M')
+                                await ctx.author.send(f'Your rent is due: {rent_timer}')
+                            else:
+                                await ctx.author.send(f'Account {account} does not appear to have a rental property.')
             else:
                 await ctx.author.send(f'Account {account} not found on this ACE server.')
 
